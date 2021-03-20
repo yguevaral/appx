dbe0041df8e4c26357d05e2409740c0a9a498c00
Yordi <yordi@advansales.com>
2021-03-19 22:08:46 -0600

    cambios lenovo

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:appx/models/mensajes_response.dart';
 import 'package:appx/services/auth_service.dart';
 import 'package:appx/services/chat_service.dart';
@@ -9,6 +11,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -21,6 +24,7 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
   final _focusNode = new FocusNode();
   bool _estaEscribiendo = false;
   final GlobalKey<NavigatorState> navigatorKeyChat = new GlobalKey<NavigatorState>();
+  File _image;
 
   ChatService chatService;
   SocketService socketService;
@@ -44,7 +48,6 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     this.socketService.socket.on('mensaje-personal', _escucharMensaje);
 
     _cargarHistorial(this.chatService.usuarioPara.uid);
-
   }
 
   void _cargarHistorial(String usuarioID) async {
@@ -164,6 +167,7 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
               ),
             ),
             Container(
+              width: MediaQuery.of(context).size.height * 0.05,
               margin: EdgeInsets.symmetric(horizontal: 4.0),
               child: Platform.isIOS
                   ? CupertinoButton(
@@ -182,6 +186,22 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
                         ),
                       ),
                     ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.height * 0.05,
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconTheme(
+                  data: IconThemeData(color: Colors.blue[400]),
+                  child: IconButton(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    icon: Icon(Icons.add_photo_alternate),
+                    onPressed: () => _showPicker(context),
+                  ),
+                ),
+              ),
             )
           ],
         ),
@@ -243,5 +263,72 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     } else {
       return Container();
     }
+  }
+
+  _imgFromCamera() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    Uint8List imageBytes = image.readAsBytesSync();
+    _textController.clear();
+    _focusNode.requestFocus();
+
+    final newMessage = new ChatMessage(
+      uid: authService.usuario.uid,
+      texto: 'image',
+      animationController: AnimationController(vsync: this, duration: Duration(milliseconds: 200)),
+      tipo: "I",
+      imageBytes: imageBytes,
+    );
+    _messages.insert(0, newMessage);
+    newMessage.animationController.forward();
+
+    //emitir mensaje
+    this.socketService.emit(
+        'mensaje-personal', {
+          'de': this.authService.usuario.uid,
+          'para': this.chatService.usuarioPara.uid,
+          'mensaje': imageBytes,
+          'tipo': "I"});
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  _imgFromGallery() async {
+    File image = await ImagePicker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Galería'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camara'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:appx/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,12 +10,11 @@ class ChatMessage extends StatelessWidget {
   final String texto;
   final String uid;
   final AnimationController animationController;
+  final String tipo;
+  final Uint8List imageBytes;
 
   const ChatMessage(
-      {Key key,
-      @required this.texto,
-      @required this.uid,
-      @required this.animationController})
+      {Key key, @required this.texto, @required this.uid, @required this.animationController, String this.tipo = 'T', this.imageBytes})
       : super(key: key);
 
   @override
@@ -21,8 +24,7 @@ class ChatMessage extends StatelessWidget {
     return FadeTransition(
       opacity: animationController,
       child: SizeTransition(
-        sizeFactor:
-            CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+        sizeFactor: CurvedAnimation(parent: animationController, curve: Curves.easeOut),
         child: Container(
           child: this.uid == authService.usuario.uid ? _myMessage() : _notMyMessage(),
         ),
@@ -31,15 +33,15 @@ class ChatMessage extends StatelessWidget {
   }
 
   Widget _myMessage() {
+    // Uint8List _image = this.texto as Uint8List;
+
     return Align(
       alignment: Alignment.centerRight,
       child: Container(
         padding: EdgeInsets.all(8.0),
         margin: EdgeInsets.only(bottom: 5.0, left: 50.0, right: 5.0),
-        child: Text(this.texto, style: TextStyle(color: Colors.white)),
-        decoration: BoxDecoration(
-            color: Color(0xff4D9Ef6),
-            borderRadius: BorderRadius.circular(20.0)),
+        child: this.tipo == "T" ? Text(this.texto, style: TextStyle(color: Colors.white)) : Image.memory(this.imageBytes),
+        decoration: BoxDecoration(color: Color(0xff4D9Ef6), borderRadius: BorderRadius.circular(20.0)),
       ),
     );
   }
@@ -50,10 +52,8 @@ class ChatMessage extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.all(8.0),
         margin: EdgeInsets.only(bottom: 5.0, right: 50.0, left: 5.0),
-        child: Text(this.texto, style: TextStyle(color: Colors.black87)),
-        decoration: BoxDecoration(
-            color: Color(0xffE4E5E8),
-            borderRadius: BorderRadius.circular(20.0)),
+        child: this.tipo == "T" ? Text(this.texto, style: TextStyle(color: Colors.black87)) : Image.memory(this.imageBytes),
+        decoration: BoxDecoration(color: Color(0xffE4E5E8), borderRadius: BorderRadius.circular(20.0)),
       ),
     );
   }
